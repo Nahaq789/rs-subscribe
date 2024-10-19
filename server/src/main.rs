0689e1aa060f33f8d0ebff50ae522ead070fa6c5
Nahaq789 <naha022677@icloud.com>
2024-10-19 22:46:47 +0900

    run app

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use tracing::{event, Level};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+  tracing_subscriber::fmt()
+    .with_max_level(Level::DEBUG)
+    .with_ansi(false)
+    .init();
+
+  event!(Level::INFO, "Application Started");
+
+  Ok(())
 }
