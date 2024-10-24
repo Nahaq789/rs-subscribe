15ac1e87a1c594f45cce72e938d9e007c6eb3669
Nahaq789 <naha022677@icloud.com>
2024-10-25 07:09:56 +0900

    add: test fix: warn

--- src/domain/src/subscribe.rs
+++ src/domain/src/subscribe.rs
@@ -1,3 +1,4 @@
+use uuid::Uuid;
 use crate::subscribe::subscribe_id::SubscribeId;
 
 pub mod subscribe_id;
@@ -8,7 +9,39 @@ pub struct Subscribe {
 }
 
 impl Subscribe {
-    fn new(subscribe_id: SubscribeId) -> Self {
-        Self { subscribe_id }
+    pub fn new() -> Self {
+        let id = SubscribeId::new();
+        Self {
+            subscribe_id: id
+        }
+    }
+
+    pub fn from(subscribe_id: Uuid) -> Self {
+        Self {
+            subscribe_id: SubscribeId::from(subscribe_id)
+        }
+    }
+
+    pub fn subscribe_id(&self) -> &SubscribeId {
+        &self.subscribe_id
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_subscribe_new_success() {
+        let result = Subscribe::new();
+        assert!(!result.subscribe_id.to_string().is_empty());
+    }
+
+    #[test]
+    fn test_subscribe_from_success() {
+        let id = Uuid::new_v4();
+        let result = Subscribe::from(id);
+        assert!(!result.subscribe_id.to_string().is_empty());
+        assert_eq!(format!("SUBSCRIBE-{}", id.to_string()), result.subscribe_id.to_string())
     }
 }
