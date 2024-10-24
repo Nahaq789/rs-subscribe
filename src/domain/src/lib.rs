b62c180df575554ef4a0860f5bad78e9ae38d68b
Nahaq789 <naha022677@icloud.com>
2024-10-24 22:56:27 +0900

    add: subscribe id

--- src/domain/src/lib.rs
+++ src/domain/src/lib.rs
@@ -1,1 +1,16 @@
+use uuid::Uuid;
+
 pub mod subscribe;
+
+/// 集約ID用のトレイトです
+///
+/// 各集約IDはAggregateIdを実装しなければなりません
+///
+/// - type_name: 集約の型を返します
+/// - value: 値を返します
+/// - generate_id: Uuidを生成します
+pub trait AggregateId {
+  fn type_name(&self) -> String;
+  fn value(&self) -> String;
+  fn generate_id() -> Uuid;
+}
--- src/domain/src/subscribe/subscribe_id.rs
+++ src/domain/src/subscribe/subscribe_id.rs
@@ -1,2 +1,49 @@
-#[derive(Debug)]
-pub struct SubscribeId {}
+use std::fmt::{Display, Formatter};
+use crate::AggregateId;
+use uuid::Uuid;
+
+#[derive(Debug, Clone)]
+pub struct SubscribeId {
+  value: Uuid
+}
+
+const SUBSCRIBE_PREFIX: &str = "SUBSCRIBE";
+
+impl SubscribeId {
+  /// コンストラクタ IDなし
+  pub fn new() -> Self {
+    let value = Self::generate_id();
+    Self { value }
+  }
+}
+
+impl AggregateId for SubscribeId {
+  fn type_name(&self) -> String {
+    SUBSCRIBE_PREFIX.to_string()
+  }
+  fn value(&self) -> String {
+    self.value.to_string()
+  }
+  fn generate_id() -> Uuid {
+    Uuid::new_v4()
+  }
+}
+
+impl From<Uuid> for SubscribeId {
+  /// 引数ありコンストラクタ
+  ///
+  /// # Argument
+  /// - `value`: Uuid
+  ///
+  /// # Return
+  /// Self
+  fn from(value: Uuid) -> Self {
+    Self { value }
+  }
+}
+
+impl Display for SubscribeId {
+  fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+    write!(f, "{}-{}", self.type_name(), self.value)
+  }
+}
