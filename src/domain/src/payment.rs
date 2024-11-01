24548929f05eff31e7bd99b3371173c10d3a86cd
Nahaq789 <naha022677@icloud.com>
2024-11-01 17:58:21 +0900

    fix: payment

--- src/domain/src/payment.rs
+++ src/domain/src/payment.rs
@@ -1,7 +1,15 @@
+mod payment_method_id;
+
+use chrono::{DateTime, Utc};
+use payment_method_id::PaymentMethodId;
+
 #[allow(dead_code)]
 #[derive(Debug, Clone)]
 pub struct PaymentMethod {
-    payment_method_id: i32,
-    payment_method_detail_id: i32,
+    payment_method_id: PaymentMethodId,
     method_name: String,
+    detail_name: String,
+    additional_name: Option<String>,
+    created_at: DateTime<Utc>,
+    updated_at: DateTime<Utc>,
 }
