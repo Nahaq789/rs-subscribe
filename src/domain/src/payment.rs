a08072baa2bcdbdf7a2be389c64f84d5f6f6351a
Nahaq789 <naha022677@icloud.com>
2024-11-05 07:48:36 +0900

    fix: later

--- src/domain/src/payment.rs
+++ src/domain/src/payment.rs
@@ -2,36 +2,36 @@ pub mod payment_method;
 pub mod payment_method_id;
 
 use chrono::{DateTime, Utc};
-use payment_method::{PaymentMethodAggregate, PaymentMethodName};
+use payment_method::{MethodNameAggregate, PaymentMethodName};
 use payment_method_id::PaymentMethodId;
 
 #[allow(dead_code)]
 #[derive(Debug, Clone)]
 pub struct PaymentMethod {
-    payment_method_id: PaymentMethodId,
-    method_name: String,
-    detail_name: String,
-    additional_name: Option<String>,
-    created_at: Option<DateTime<Utc>>,
-    updated_at: Option<DateTime<Utc>>,
+  payment_method_id: PaymentMethodId,
+  method_name: String,
+  detail_name: String,
+  additional_name: Option<String>,
+  created_at: Option<DateTime<Utc>>,
+  updated_at: Option<DateTime<Utc>>,
 }
 
 impl PaymentMethod {
-    pub fn new(
-        payment_method_id: PaymentMethodId,
-        method_name: PaymentMethodAggregate,
-        additional_name: Option<String>,
-        created_at: Option<DateTime<Utc>>,
-        updated_at: Option<DateTime<Utc>>,
-    ) -> Self {
-        let (method_name, detail_name) = PaymentMethodName::new(method_name).get_method_detail_name();
-        Self {
-            payment_method_id,
-            method_name,
-            detail_name,
-            additional_name,
-            created_at,
-            updated_at,
-        }
+  pub fn new(
+    payment_method_id: PaymentMethodId,
+    method_name: MethodNameAggregate,
+    additional_name: Option<String>,
+    created_at: Option<DateTime<Utc>>,
+    updated_at: Option<DateTime<Utc>>,
+  ) -> Self {
+    let (method_name, detail_name) = PaymentMethodName::new(method_name).get_method_detail_name();
+    Self {
+      payment_method_id,
+      method_name,
+      detail_name,
+      additional_name,
+      created_at,
+      updated_at,
     }
+  }
 }
