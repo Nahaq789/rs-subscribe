35a456b105275e729108436f61602c2245c2c776
Nahaq789 <naha022677@icloud.com>
2024-11-10 23:21:21 +0900

    fix: fmt

--- src/domain/src/payment/payment_method.rs
+++ src/domain/src/payment/payment_method.rs
@@ -230,40 +230,101 @@ mod tests {
         let test_case = vec![
             // Credit Card
             ("Visa", PaymentMethodName::CreditCard(CreditCard::Visa)),
-            ("MasterCard", PaymentMethodName::CreditCard(CreditCard::MasterCard)),
-            ("American Express", PaymentMethodName::CreditCard(CreditCard::AmericanExpress)),
+            (
+                "MasterCard",
+                PaymentMethodName::CreditCard(CreditCard::MasterCard),
+            ),
+            (
+                "American Express",
+                PaymentMethodName::CreditCard(CreditCard::AmericanExpress),
+            ),
             ("JCB", PaymentMethodName::CreditCard(CreditCard::JCB)),
-            ("Discover", PaymentMethodName::CreditCard(CreditCard::Discover)),
-            ("Diners Club", PaymentMethodName::CreditCard(CreditCard::DinersClub)),
-
+            (
+                "Discover",
+                PaymentMethodName::CreditCard(CreditCard::Discover),
+            ),
+            (
+                "Diners Club",
+                PaymentMethodName::CreditCard(CreditCard::DinersClub),
+            ),
             // Digital Money
-            ("Suica", PaymentMethodName::DigitalMoney(DigitalMoney::Suica)),
-            ("PASMO", PaymentMethodName::DigitalMoney(DigitalMoney::Pasmo)),
-            ("nanaco", PaymentMethodName::DigitalMoney(DigitalMoney::Nanaco)),
+            (
+                "Suica",
+                PaymentMethodName::DigitalMoney(DigitalMoney::Suica),
+            ),
+            (
+                "PASMO",
+                PaymentMethodName::DigitalMoney(DigitalMoney::Pasmo),
+            ),
+            (
+                "nanaco",
+                PaymentMethodName::DigitalMoney(DigitalMoney::Nanaco),
+            ),
             ("WAON", PaymentMethodName::DigitalMoney(DigitalMoney::Waon)),
-            ("楽天Edy", PaymentMethodName::DigitalMoney(DigitalMoney::RakutenEdy)),
-
+            (
+                "楽天Edy",
+                PaymentMethodName::DigitalMoney(DigitalMoney::RakutenEdy),
+            ),
             // Mobile Payment
-            ("PayPay", PaymentMethodName::MobilePayment(MobilePayment::PayPay)),
-            ("LinePay", PaymentMethodName::MobilePayment(MobilePayment::LinePay)),
-            ("MerPay", PaymentMethodName::MobilePayment(MobilePayment::MerPay)),
-            ("RakutenPay", PaymentMethodName::MobilePayment(MobilePayment::RakutenPay)),
-            ("DBarai", PaymentMethodName::MobilePayment(MobilePayment::DBarai)),
-            ("Venmo", PaymentMethodName::MobilePayment(MobilePayment::Venmo)),
-            ("CashApp", PaymentMethodName::MobilePayment(MobilePayment::CashApp)),
-            ("Zelle", PaymentMethodName::MobilePayment(MobilePayment::Zelle)),
-            ("PayPal", PaymentMethodName::MobilePayment(MobilePayment::PayPal)),
-
+            (
+                "PayPay",
+                PaymentMethodName::MobilePayment(MobilePayment::PayPay),
+            ),
+            (
+                "LinePay",
+                PaymentMethodName::MobilePayment(MobilePayment::LinePay),
+            ),
+            (
+                "MerPay",
+                PaymentMethodName::MobilePayment(MobilePayment::MerPay),
+            ),
+            (
+                "RakutenPay",
+                PaymentMethodName::MobilePayment(MobilePayment::RakutenPay),
+            ),
+            (
+                "DBarai",
+                PaymentMethodName::MobilePayment(MobilePayment::DBarai),
+            ),
+            (
+                "Venmo",
+                PaymentMethodName::MobilePayment(MobilePayment::Venmo),
+            ),
+            (
+                "CashApp",
+                PaymentMethodName::MobilePayment(MobilePayment::CashApp),
+            ),
+            (
+                "Zelle",
+                PaymentMethodName::MobilePayment(MobilePayment::Zelle),
+            ),
+            (
+                "PayPal",
+                PaymentMethodName::MobilePayment(MobilePayment::PayPal),
+            ),
             // Digital Wallet
-            ("ApplePay", PaymentMethodName::DigitalWallet(DigitalWallet::ApplePay)),
-            ("GooglePay", PaymentMethodName::DigitalWallet(DigitalWallet::GooglePay)),
-            ("AmazonPay", PaymentMethodName::DigitalWallet(DigitalWallet::AmazonPay)),
-
+            (
+                "ApplePay",
+                PaymentMethodName::DigitalWallet(DigitalWallet::ApplePay),
+            ),
+            (
+                "GooglePay",
+                PaymentMethodName::DigitalWallet(DigitalWallet::GooglePay),
+            ),
+            (
+                "AmazonPay",
+                PaymentMethodName::DigitalWallet(DigitalWallet::AmazonPay),
+            ),
             // Bank Transfer
-            ("JapaneseBankTransfer", PaymentMethodName::BankTransfer(BankTransfer::JapaneseBankTransfer)),
-            ("JapaneseDirectDebit", PaymentMethodName::BankTransfer(BankTransfer::JapaneseDirectDebit)),
+            (
+                "JapaneseBankTransfer",
+                PaymentMethodName::BankTransfer(BankTransfer::JapaneseBankTransfer),
+            ),
+            (
+                "JapaneseDirectDebit",
+                PaymentMethodName::BankTransfer(BankTransfer::JapaneseDirectDebit),
+            ),
             ("ACH", PaymentMethodName::BankTransfer(BankTransfer::ACH)),
-
             // BNPL
             ("Affirm", PaymentMethodName::BNPL(BNPL::Affirm)),
             ("Klarna", PaymentMethodName::BNPL(BNPL::Klarna)),
@@ -289,7 +350,11 @@ mod tests {
         ];
 
         for input in error_cases {
-            assert!(PaymentMethodName::from_str(input).is_err(), "input: {}", input)
+            assert!(
+                PaymentMethodName::from_str(input).is_err(),
+                "input: {}",
+                input
+            )
         }
     }
 }
