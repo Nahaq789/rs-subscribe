dd9b64d1011f7d59419992211a487938ad307525
Nahaq789 <naha022677@icloud.com>
2024-11-24 20:52:30 +0900

    fix: payment method name

--- src/domain/src/payment/payment_error.rs
+++ src/domain/src/payment/payment_error.rs
@@ -1,3 +1,5 @@
+use crate::payment::payment_method_name::PaymentMethodNameError;
+use crate::AggregateIdError;
 use thiserror::Error;
 
 #[derive(Debug, Error)]
@@ -7,4 +9,25 @@ pub enum PaymentError {
 
   #[error("Failed to delete payment method: {0}")]
   DeletePaymentMethodFailed(String),
+
+  #[error("Failed to query payment methods: {0}")]
+  QueryError(String),
+
+  #[error("{0}")]
+  PaymentMethodIdFailed(String),
+
+  #[error("{0}")]
+  PaymentMethodNameFailed(String),
+}
+
+impl From<AggregateIdError> for PaymentError {
+  fn from(value: AggregateIdError) -> Self {
+    PaymentError::PaymentMethodIdFailed(value.to_string())
+  }
+}
+
+impl From<PaymentMethodNameError> for PaymentError {
+  fn from(value: PaymentMethodNameError) -> Self {
+    PaymentError::PaymentMethodNameFailed(value.to_string())
+  }
 }
