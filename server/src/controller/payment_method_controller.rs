d73f4a31509b2d39cbb370bb0d41e812dd4e5116
Nahaq789 <naha022677@icloud.com>
2024-12-08 20:35:19 +0900

    feat: find all payment method

--- server/src/controller/payment_method_controller.rs
+++ server/src/controller/payment_method_controller.rs
@@ -1,5 +1,6 @@
 use crate::app_state::PaymentMethodState;
 use application::error::ApplicationError;
+use axum::extract::Path;
 use axum::http::StatusCode;
 use axum::response::{IntoResponse, Response};
 use axum::{Extension, Json};
@@ -30,13 +31,20 @@ pub async fn create_payment_method(
   Extension(module): Extension<PaymentMethodState>,
   // Json(payload): Json<PaymentMethodDTO>,
 ) -> Result<impl IntoResponse, ApplicationErrorWrapper> {
-  // create argument
-  // call payment service
-  // create response
-  // make error handling
-
   let user_id = "usr_514ac014-fa95-0bae-437e-e5998be7be3c";
   let result = module.state.find_payment_method_all(user_id).await;
+
+  match result {
+    Ok(v) => Ok((StatusCode::OK, Json(v))),
+    Err(e) => Err(ApplicationErrorWrapper(e)),
+  }
+}
+
+pub async fn find_payment_method_all(
+  Extension(module): Extension<PaymentMethodState>,
+  Path(user_id): Path<String>,
+) -> Result<impl IntoResponse, ApplicationErrorWrapper> {
+  let result = module.state.find_payment_method_all(&user_id).await;
 
   match result {
     Ok(v) => Ok((StatusCode::OK, Json(v))),
