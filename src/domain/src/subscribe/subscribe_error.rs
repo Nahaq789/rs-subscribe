f2d11cd44253915304bcbacacdbbb25716ab560f
Nahaq789 <naha022677@icloud.com>
2024-12-19 08:21:11 +0900

    add: subscribe error another variants

--- src/domain/src/subscribe/subscribe_error.rs
+++ src/domain/src/subscribe/subscribe_error.rs
@@ -1,4 +1,4 @@
-use crate::value_object::amount::AmountError;
+use crate::{value_object::amount::AmountError, AggregateIdError};
 use thiserror::Error;
 
 /// サブスクリプション操作に関するエラー
@@ -16,4 +16,34 @@ pub enum SubscribeError {
 
   #[error("Not match Subscribe Status: {0}")]
   InvalidSubscribeStatus(String),
+
+  #[error("Failed to delete subscribe: {0}")]
+  DeleteSubscribeFailed(String),
+
+  #[error("Failed to query subscribe: {0}")]
+  QueryError(String),
+
+  #[error("Failed to find by id subscribe: {0}")]
+  FindByIdError(String),
+
+  #[error("Failed to update subscribe: {0}")]
+  UpdateSubscribeError(String),
+
+  #[error("Required subscribe field '{0}' was missing")]
+  MissingField(String),
+
+  #[error("Failed to create subscribe: {0}")]
+  CreateSubscribeFailed(String),
+
+  #[error("Subscribe not exist")]
+  NotExists,
+
+  #[error("{0}")]
+  SubscribeIdFailed(String),
+}
+
+impl From<AggregateIdError> for SubscribeError {
+  fn from(value: AggregateIdError) -> Self {
+    SubscribeError::SubscribeIdFailed(value.to_string())
+  }
 }
