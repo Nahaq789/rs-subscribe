ce631bebb3a4bed17725ebfe8d74e143ab61088d
Nahaq789 <naha022677@icloud.com>
2025-01-24 18:54:32 +0900

    0124

--- server/src/app_state.rs
+++ server/src/app_state.rs
@@ -1,8 +1,10 @@
 use crate::app_state::StateError::BuildError;
 use crate::client::{Database, DatabaseBuilder};
 use application::service::payment_method_service::PaymentMethodServiceImpl;
-use application::service::PaymentMethodService;
+use application::service::subscribe_service::SubscribeServiceImpl;
+use application::service::{PaymentMethodService, SubscribeService};
 use infrastructure::repository_impl::payment_repository_impl::PaymentRepositoryImpl;
+use infrastructure::repository_impl::subscribe_repository_impl::SubscribeRepositoryImpl;
 use std::sync::Arc;
 use thiserror::Error;
 
@@ -13,6 +15,7 @@ pub enum StateError {
 }
 
 pub type DynPaymentService = Arc<dyn PaymentMethodService + Send + Sync>;
+pub type DynSubscribeService = Arc<dyn SubscribeService>;
 
 #[derive(Clone)]
 pub struct PaymentMethodState {
@@ -29,6 +32,26 @@ impl PaymentMethodState {
 
         let repository = PaymentRepositoryImpl::new(client.client(), table);
         let service = PaymentMethodServiceImpl::new(repository);
+
+        Ok(Self { state: Arc::new(service) })
+    }
+}
+
+#[derive(Clone)]
+pub struct SubscribeState {
+    pub state: DynSubscribeService,
+}
+
+impl SubscribeState {
+    pub async fn new(table: &str) -> Result<Self, StateError> {
+        let build = Database::build(None).await;
+        let client = match build {
+            Ok(b) => b,
+            Err(e) => return Err(BuildError(e.to_string())),
+        };
+
+        let repository = SubscribeRepositoryImpl::new(client.client(), table);
+        let service = SubscribeServiceImpl::new(repository);
 
         Ok(Self { state: Arc::new(service) })
     }
---
+++ server/src/controller/subscribe_controller.rs
@@ -0,0 +1,25 @@
+use application::dtos::subscribe_dto::SubscribeDto;
+use axum::{http::StatusCode, response::IntoResponse, Extension, Json};
+use serde_json::json;
+
+use crate::app_state::SubscribeState;
+
+use super::ApplicationErrorWrapper;
+
+pub async fn create_subscribe(
+    Extension(module): Extension<SubscribeState>,
+    Json(payload): Json<SubscribeDto>,
+) -> Result<impl IntoResponse, ApplicationErrorWrapper> {
+    let result = module.state.create_subscribe(payload).await;
+    let response = json!(
+       {
+    "message": "subscribe created",
+        "status code": StatusCode::OK.as_u16()
+       }
+    );
+
+    match result {
+        Ok(_) => Ok((StatusCode::OK, Json(response))),
+        Err(e) => Err(ApplicationErrorWrapper(e)),
+    }
+}
