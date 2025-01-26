a31c7e0249630897189e37e5171e5270fdd99a6a
Nahaq789 <naha022677@icloud.com>
2025-01-26 19:21:56 +0900

    fix: terraform code

--- terraform/modules/dynamodb/variables.tf
+++ terraform/modules/dynamodb/variables.tf
@@ -4,10 +4,16 @@ variable "environment" {
 
 variable "tables" {
   type = map(object({
-    hash_key = string
+    hash_key       = string
     range_key = optional(string)
-    read_capacity = number
+    read_capacity  = number
     write_capacity = number
     attributes = map(string)
+    gsis = optional(map(object({
+      hash_key        = string
+      range_key       = string
+      name            = string
+      projection_type = string
+    })))
   }))
 }
