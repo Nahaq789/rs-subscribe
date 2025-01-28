a966a3c4ae4dbb76a8de8cef833438d4eb44ddbf
Nahaq789 <naha022677@icloud.com>
2025-01-28 23:36:53 +0900

    category params

--- server/src/controller/params.rs
+++ server/src/controller/params.rs
@@ -1,2 +1,3 @@
+pub mod category_params;
 pub mod payment_method_params;
 pub mod subscribe_params;
---
+++ server/src/controller/params/category_params.rs
@@ -0,0 +1,12 @@
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+pub struct FindAllParam {
+    pub user_id: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct FindByIdParams {
+    pub user_id: String,
+    pub subscribe_id: String,
+}
